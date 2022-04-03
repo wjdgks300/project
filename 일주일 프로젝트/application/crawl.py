b881364c6c862c464222bd04ef8f8cb2162e4b8e
leejeonghan <wjdgks300@naver.com>
2022-04-03 17:31:07 +0900

    sixth

---
+++ 일주일 프로젝트/application/crawl.py
@@ -0,0 +1,118 @@
+import random
+import threading
+import time
+import pandas as pd
+import requests
+from tqdm import tqdm
+import lxml.html
+import numpy as np
+
+# 첫회가 무엇인지 받아오기
+def first():
+    req_urla = "https://www.dhlottery.co.kr/gameResult.do?method=byWin"
+    req_lottoa = requests.get(req_urla)
+    html = req_lottoa.text
+    root = lxml.html.fromstring(html)
+    num = root.xpath('//*[@id="article"]/div[2]/div/div[2]/h4/strong')
+    thisnum = num[0].text
+    print(thisnum.strip("회"))
+    return thisnum
+
+
+def getLottoWinInfo(startRound, endRound):
+    drwtNo1 = []
+    drwtNo2 = []
+    drwtNo3 = []
+    drwtNo4 = []
+    drwtNo5 = []
+    drwtNo6 = []
+    bnusNo = []
+    totSellamnt = []
+    drwNoDate = []
+    firstAccumamnt = []
+    firstPrzwnerCo = []
+    firstWinamnt = []
+    roundNo = []
+    for i in tqdm(range(startRound, endRound+1, 1)): # i = 1
+        req_url = "https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=" + str(i)
+        req_lotto = requests.get(req_url)
+        lottoNo = req_lotto.json()
+        drwtNo1.append(lottoNo['drwtNo1'])
+        drwtNo2.append(lottoNo['drwtNo2'])
+        drwtNo3.append(lottoNo['drwtNo3'])
+        drwtNo4.append(lottoNo['drwtNo4'])
+        drwtNo5.append(lottoNo['drwtNo5'])
+        drwtNo6.append(lottoNo['drwtNo6'])
+        bnusNo.append(lottoNo['bnusNo'])
+        roundNo.append(i)
+        totSellamnt.append(lottoNo['totSellamnt'])
+        drwNoDate.append(lottoNo['drwNoDate'])
+        firstAccumamnt.append(lottoNo['firstAccumamnt'])
+        firstPrzwnerCo.append(lottoNo['firstPrzwnerCo'])
+        firstWinamnt.append(lottoNo['firstWinamnt'])
+        lotto_dict = {"추첨일":drwNoDate, "회차":roundNo, "Num1":drwtNo1, "Num2":drwtNo2, "Num3":drwtNo3, "Num4":drwtNo4, "Num5":drwtNo5, "Num6":drwtNo6, "bnsNum":bnusNo, "총판매금액":totSellamnt, "총1등당첨금":firstAccumamnt, "1등당첨인원":firstPrzwnerCo, "1등수령액":firstWinamnt}
+        lotto_df = pd.DataFrame(lotto_dict)
+    return lotto_dict, lotto_df
+    #%% 로또 회차정보 불러오기
+
+thisnum = first()
+endRound =int(thisnum.strip("회"))
+startRound = endRound - 1
+
+
+lotto_dict, lotto_df = getLottoWinInfo(startRound, endRound)
+
+
+
+# CSV 파일로 저장
+#lotto_df.to_csv(str(startRound) + '-' + str(endRound) + '_lotto_number.csv', index = False, encoding = 'utf-8-sig')
+#csv_test = pd.read_csv(str(startRound) + '-' + str(endRound) + '_lotto_number.csv')
+proba = np.zeros([1,46])
+
+array_test = lotto_df.to_numpy()
+# print(proba)
+
+#반복문으로 1~45까지 어떤 수가 나왔는지 count
+for j in range(1,len(array_test)):
+    for i in range(2,9):
+        for k in range(1,46):
+            if array_test[j][i] == k:
+                proba[0][k] = proba[0][k] + 1
+
+#1등 당첨 number
+this_winninnum = array_test[endRound- startRound][2:9]
+
+#1등 당첨 금액
+this_winamnt = array_test[endRound- startRound][10:13]
+print(this_winamnt)
+
+#확률 구하기
+percentage = 0
+weight = np.zeros([45])
+
+for i in range(1,46):
+    percentage += (proba[0][i] / np.sum(proba))
+    weight[i-1] = proba[0][i] / np.sum(proba)
+    #print(i ," = ", proba[0][i] / np.sum(proba) , " 총 개수 =" ,np.sum(proba)," " ,i," 개수 = " ,proba[0][i])
+
+#print(percentage)
+print(proba)
+print(weight)
+
+
+#랜덤 추출하기(확률에 따라) change method -> while len(lotto) < 6 ...
+select_index = []
+while len(select_index) < 7:
+    num = random.choices(range(1,46), weights= weight)
+    if not num in select_index:
+        select_index.append(num)
+
+print(select_index)
+#print(csv_test.shape)
+
+
+#일정 시간마다 반복 출력하기
+def timer():
+    getLottoWinInfo(0,51)
+    threading.Timer(100,timer).start()
+
--- 일주일 프로젝트/application/main.py
+++ 일주일 프로젝트/application/main.py
@@ -1,9 +1,22 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen, ScreenManager
-from kivy.properties import StringProperty
+from kivy.properties import StringProperty, NumericProperty, ObjectProperty
+from crawl import *
+
+
 
 class LottoScreen(Screen):
+    one = NumericProperty(select_index[0][0])
+    two = NumericProperty(select_index[1][0])
+    three = NumericProperty(select_index[2][0])
+    four = NumericProperty(select_index[3][0])
+    five = NumericProperty(select_index[4][0])
+    six = NumericProperty(select_index[5][0])
+    seven = NumericProperty(select_index[6][0])
+    this_win = ObjectProperty(this_winninnum)
+    this_prize = ObjectProperty(this_winamnt)
+    this_round = NumericProperty(endRound)
     pass
 class HomeScreen(Screen):
     pass
@@ -14,9 +27,9 @@ x = 1
 
 GUI = Builder.load_file("main.kv")
 class MainApp(App):
-    a = StringProperty("test")
+
     def build(self):
-        self.a = StringProperty("test")
+
         return GUI
 
     def change_screen(self, screen_name):
